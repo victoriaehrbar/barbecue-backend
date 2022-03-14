43d5105e4d662ae56b39b8fb12ed052fc45e542c
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-14 17:38:33 -0500

    commented out destroy

--- app/controllers/barbecues_controller.rb
+++ app/controllers/barbecues_controller.rb
@@ -1,20 +1,29 @@
 class BarbecuesController < ApplicationController
+    
+    #GET
     def index
         barbecues = Barbecue.all
         render json: barbecues
     end
 
+    #GET
     def show
         barbecue = Barbecue.find(params[:id])
         render json: barbecue
     end
 
+    #POST
     def create
         barbecue = Barbecue.new(barbecue_params)
         if barbecue.save
             render json: barbecue
         end
     end
+
+    #DELETE
+    # def destroy
+    #     @barbecue.destroy
+    # end
 
     private
 
