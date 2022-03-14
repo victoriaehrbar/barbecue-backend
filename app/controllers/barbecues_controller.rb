aa09f3ebd84297b1eeb76876212db6e102e68f61
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-14 17:28:27 -0500

    controller

--- app/controllers/barbecues_controller.rb
+++ app/controllers/barbecues_controller.rb
@@ -1,2 +1,25 @@
 class BarbecuesController < ApplicationController
+    def index
+        barbecues = Barbecue.all
+        render json: barbecues
+    end
+
+    def show
+        barbecue = Barbecue.find(params[:id])
+        render json: barbecue
+    end
+
+    def create
+        barbecue = Barbecue.new(Barbecue_params)
+        if barbecue.save
+            render json: barbecue
+        end
+    end
+
+    private
+
+    def barbecue_params
+        params.require(:barbecue).permit(:name, :rating, :review)
+    end
+
 end
